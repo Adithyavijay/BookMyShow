c67c2b7775063db77cc64476a7fd94af66e3c8b6
adithya vijay <adithyavijay99@gmail.com>
2025-01-14 10:15:03 +0530

    update origin

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://bookmyshow.entebuddy.in', // Allows request from 3000
+  origin: 'https://bookmyshow.entebuddy.in',  
   credentials: true,
 }));
 
